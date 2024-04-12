cfe76f790fb07372f916c134c3bc7a61bf297389
Peter Logo <peterlogo77@gmail.com>
2024-04-12 12:20:09 -0400

    Added update on logger

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import Fastify from 'fastify';
 import { config } from './config';
 
-const server = Fastify();
+const server = Fastify({
+  logger: true
+});
 
 server.get('/', async (_, reply) => {
   reply.code(200).send({ message: 'Hello, world!' });
 });
 
-server.listen({ port: config.PORT, host: '0.0.0.0' }, (err, address) => {
+server.listen({ port: config.PORT, host: '0.0.0.0' }, (err, __) => {
   if (err) {
-    console.error(err);
+    server.log.error(err);
     process.exit(1);
   }
-
-  console.log(`Server listening at ${address}`);
 });
