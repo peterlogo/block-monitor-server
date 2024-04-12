6874d64cc63c194b16782662043dc47e35a6c807
Peter Logo <peterlogo77@gmail.com>
2024-04-12 09:44:56 -0400

    Added development environment

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import jestPlugin from 'eslint-plugin-jest';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default tseslint.config(
+  {
+    // config with just ignores is the replacement for `.eslintignore`
+    ignores: ['**/dist/**', '**/node_modules/**']
+  },
+  eslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      jest: jestPlugin
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-unsafe-argument': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-unsafe-call': 'error',
+      '@typescript-eslint/no-unsafe-member-access': 'error',
+      '@typescript-eslint/no-unsafe-return': 'error'
+    }
+  },
+  {
+    // disable type-aware linting on JS files
+    files: ['**/*.js'],
+    ...tseslint.configs.disableTypeChecked
+  },
+  {
+    // enable jest rules on test files
+    files: ['tests/**'],
+    ...jestPlugin.configs['flat/recommended']
+  }
+);
